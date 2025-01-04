a80d5c9e6bdf80f38a89fdd906ca324c92606469
Mritunjay-maker <mritunjayjha2014@gmail.com>
2025-01-04 15:10:52 +0530

    added bs feature

--- firstProgram.cpp
+++ firstProgram.cpp
@@ -1,8 +1,31 @@
 #include<iostream>
 using namespace std;
 
+int binarySearch(int n, int arr[], int element) {
+    int start = 0, end = n - 1;
+    while(start <= end ){
+        int mid = start + (end - start) / 2;
+        if(arr[mid] == element) {
+            return mid;
+        }
+        else if (arr[mid] > element) {
+            end = mid - 1;
+        }
+        else{
+            start = mid + 1;
+        }
+    }
+    return -1;
+}
 
 int main() {
-    cout << "It is a beautiful day";
+    int n;
+    cin >> n;
+
     
+    int element = 4;
+    int arr[] = {1,2,3,4,5,6,7,8,9,12};
+    cout << binarySearch(10,arr,element);
+    
+
 }
