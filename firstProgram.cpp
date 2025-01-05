de349cc506a2ed456c96d89ae9c35cea96bcad72
Mritunjay-maker <mritunjayjha2014@gmail.com>
2025-01-05 10:46:25 +0530

    Added bubble Sort Code

--- firstProgram.cpp
+++ firstProgram.cpp
@@ -17,16 +17,35 @@ int binarySearch(int n, int arr[], int element) {
     }
     return -1;
 }
+void bubbleSort(int arr[], int n) {
 
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i -1; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
+void printArray(int arr[], int n) {
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] <<" ";
+    }
+}
 int main() {
     int n;
     cin >> n;
-    cout <<"This is binary Search Program";
+    
 
     
-    int element = 4;
-    int arr[] = {1,2,3,4,5,6,7,8,9,12};
-    cout << binarySearch(10,arr,element);
+    
+    int arr[] = {6, 9, 4, 8, 3, 1};
+    bubbleSort(arr,6);
+    //printArray(arr,n);
+    
+    
     
 
 }
